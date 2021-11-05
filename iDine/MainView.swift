681b8b1b244cfdaf726f10f12b98cfb9afc86050
hammer-zren <hammer.zren@gmail.com>
2021-11-05 17:38:36 +1100

    add badge

--- iDine/MainView.swift
+++ iDine/MainView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    private var badgePosition: CGFloat = 2
+    
+    private var tabsCount: CGFloat = 2
+    
     var body: some View {
-        TabView {
-            ContentView()
-                .tabItem {
-                    Label("Menu", systemImage: "list.dash")
+        GeometryReader { geometry in
+            ZStack(alignment: .bottomLeading) {
+                TabView {
+                    ContentView()
+                        .tabItem {
+                            Label("Menu", systemImage: "list.dash")
+                        }
+                    OrderView()
+                        .tabItem {
+                            Label("Order", systemImage: "cart")
+                        }
                 }
-            OrderView()
-                .tabItem {
-                    Label("Order", systemImage: "square.and.pencil")
+                
+                // Badge View
+                ZStack {
+                    Circle()
+                        .foregroundColor(.red)
+
+                    Text("\(self.order.items.count)")
+                        .foregroundColor(.white)
+                        .font(Font.system(size: 12))
                 }
+                .frame(width: 15, height: 15)
+                .offset(x: ((2 * self.badgePosition) - 0.95) * (geometry.size.width / (2 * self.tabsCount)) - 7, y: -30)
+                .opacity(self.order.items.count == 0 ? 0 : 1.0)
+            }
         }
+        
     }
 }
 
