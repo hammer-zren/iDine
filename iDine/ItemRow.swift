289196e25201b9a141bde332b48a8fc6c651c15e
hammer-zren <hammer.zren@gmail.com>
2021-11-05 11:52:53 +1100

    create menu

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,53 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by ZHONGTAO REN on 5/11/21.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "V": .green]
+    
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+            
+            VStack(alignment: .leading){
+                Text(item.name)
+                    .font(.headline)
+                Text("$\(item.price)")
+            }
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+            }
+            
+//            Text(item.name)
+//            Spacer()
+//            Text("$\(item.price)")
+            
+        }
+        
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
