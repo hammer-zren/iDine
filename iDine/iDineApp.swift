ecc128c65c21afd51610d72a57409f46ff570027
hammer-zren <hammer.zren@gmail.com>
2021-11-05 11:01:06 +1100

    setup project for swiftUI demo app

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by ZHONGTAO REN on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
